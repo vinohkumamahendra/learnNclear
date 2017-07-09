a0a73f91c59f8efe5fc18b90d42de8dded91b9cc
Vinoth Kumar Mahendira <vinothkumarmahendra@gmail.com>
2017-07-09 21:01:09 +0530

    9July17 Commit

---
+++ SeleniumProject/src/main/java/Utility/SeleniumUtility.java
@@ -0,0 +1,255 @@
+package Utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class SeleniumUtility {
+	
+	RemoteWebDriver driver;
+	WebElement element = null;
+	String parentWindowID = null;
+	boolean windowPresence = false;
+	WebDriver childWindowdriver = null;
+	
+	/**
+	 * This method launches required browser and loads the URL in it.
+	 * @param browserName
+	 * @param URLAddress
+	 */
+	public void launchBrowserAndLoadURL(String browserName, String URLAddress){
+		if(browserName.equalsIgnoreCase("firefox")){
+			System.setProperty("webdriver.gecko.driver","Driver/geckodriver.exe");
+			driver = new FirefoxDriver();			
+		}else if(browserName.equalsIgnoreCase("internet")){
+			System.setProperty("webdriver.ie.driver", "Driver/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}else if(browserName.equalsIgnoreCase("chrome")){
+			System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+			driver = new ChromeDriver();
+		}else{
+			try {
+				throw new Exception("invalid browser name");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(URLAddress);
+	}
+	public void Ale() throws Exception{
+		Alert ale=driver.switchTo().alert();
+		ale.accept();
+		
+	}
+	
+	
+	public void OpenURL(String Url){
+		try {
+			driver.get(Url);
+		} catch (Exception e) {
+			Assert.fail("Unable to open URL " + Url);
+		}
+	}
+	
+	public WebElement locateElement(String locatorType, String locator) throws Exception{
+		try {
+			if (locatorType.equalsIgnoreCase("id")) {
+				element=driver.findElement(By.id(locator));
+			} else if (locatorType.equalsIgnoreCase("name")) {
+				element=driver.findElement(By.name(locator));
+			} else if (locatorType.equalsIgnoreCase("class")) {
+				element=driver.findElement(By.className(locator));
+			} else if (locatorType.equalsIgnoreCase("xpath")) {
+				element=driver.findElement(By.xpath(locator));
+			}else if(locatorType.equalsIgnoreCase("css")){
+				element=driver.findElement(By.cssSelector(locator));
+			}else{
+				throw new Exception("invalid locator");
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			throw new Exception(e.getMessage());
+		}
+	
+		return element;
+	}
+	
+	/**
+	 * Handles text box
+	 * @param locatorType id,name,class,xpath,css
+	 * @param locator
+	 * @param textValue
+	 * @throws Exception 
+	 */
+	public void textBoxHandling(String locatorType, String locator, String textValue) throws Exception{
+		try {
+			//locateElement(locatorType, locator).sendKeys(textValue);
+			element = locateElement(locatorType, locator);
+			element.sendKeys(textValue);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	public void dropDownHandling(String locatorType, String locator, int valueToBeSelected) throws Exception{
+		locateElement(locatorType, locator);
+		
+		try {
+			Select select = new Select(element);
+			select.selectByIndex(valueToBeSelected);
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	public void buttonHandling(String locatorType, String locator) throws Exception{
+		locateElement(locatorType, locator).click();
+	}	
+	
+	
+	
+	public void TakeScreenshot(String path){
+		try {
+			File sFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(sFile, new File(path));
+		} catch (Exception e) {
+			Assert.fail("Unable to take screenshot");
+		}
+	}
+	
+	public void checkBoxHandling(String locatorType, String locator){
+		try {
+			element=locateElement(locatorType, locator);
+			if(!element.isSelected()){
+				element.click();
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public WebDriver windowHandling(String titleOfExpectedWindow){
+		parentWindowID = driver.getWindowHandle();
+		System.out.println("parent window ID " + parentWindowID);
+		
+		// get all window IDs
+		Set<String> windowIDs = driver.getWindowHandles();
+
+		/**
+		 * loop thru window IDs and switch to each window & do getTitle action to verify whether the current window title
+		 * matches with expected window title
+		 *  
+		 */
+		
+		for (String windowID : windowIDs) {
+			System.out.println(windowID);
+			
+			childWindowdriver = driver.switchTo().window(windowID);
+			String title = childWindowdriver.getTitle();
+			System.out.println("TITLE is " + title);
+			
+			if (title.contains(titleOfExpectedWindow)) {
+				break;
+			}
+
+		}
+		
+		return childWindowdriver;
+
+	}
+
+	public void goBackParentWindow(String titleOfExpectedWindow){
+		childWindowdriver.switchTo().window(parentWindowID);
+	}
+
+	public void framesHandling(String locatorType, String locator, String titleOfExpectedWindow) throws Exception{
+		driver.switchTo().frame(locateElement(locatorType, locator));
+	}
+	
+	public void goOutOfFrame(String titleOfExpectedWindow){
+		driver.switchTo().defaultContent();
+	}
+	public void screenShot(){
+//		Random random = new Random();
+//		random.ints(4444, 5555);
+		
+/**		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(file, new File("C:\\screenShot"+random.nextInt()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+		
+		
+		
+		try {
+			File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(file, new File("C:\\Users\\njayapal\\Desktop\\screenShot.png"));
+		} catch (WebDriverException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public String readData(String keyName, String fileName) {
+		String value = null;
+		try {
+
+			Properties prop = new Properties();
+			File file = new File(fileName);
+
+			if (file.exists()) {
+				FileInputStream fileinput = new FileInputStream(file);
+				prop.load(fileinput);
+
+				value = prop.getProperty(keyName);
+			} 
+				
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		return value;
+	}
+	//added to new method to pause and close browser - Vinoth
+	public void pause(int ms){
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			//cleanup
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	public void closeBrowser(){
+		driver.close();
+	}
+
+}
---
+++ SeleniumProject/src/test/java/AddCustomer/Editaddress.java
@@ -0,0 +1,111 @@
+package AddCustomer;
+
+import java.io.File;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import Utility.SeleniumUtility;
+
+public class Editaddress {
+	static SeleniumUtility a=null;
+	static RemoteWebDriver d;
+	
+	ExtentReports reports ;
+	ExtentTest test;
+	
+	@Parameters({"browser"})
+	@Test
+	public void mEditCustomer(String browser) throws Exception {	
+//		RemoteWebDriver driver = null;
+		reports = new ExtentReports("Reports/LearnNClear.html");
+		reports.addSystemInfo("Testing", "Automation testing");
+		reports.loadConfig(new File("extent2.xml"));
+		test = reports.startTest("Create User");
+		a= new SeleniumUtility();
+		a.launchBrowserAndLoadURL(browser, "http://learnnshine.com/drupal/node/add/customers");
+		a.textBoxHandling("id","edit-title","Vignesh");
+		a.textBoxHandling("id","edit-field-middle-name-und-0-value","Thevendriya");
+		a.textBoxHandling("id","edit-field-last-name-und-0-value","Anandam");
+		a.locateElement("xpath", ".//*[@id='edit-field-dob-und-0-value-datepicker-popup-0']").clear();
+		a.textBoxHandling("xpath",".//*[@id='edit-field-dob-und-0-value-datepicker-popup-0']","10/29/1988");
+		a.buttonHandling("id","edit-field-gender-und-male");
+		a.textBoxHandling("id","edit-field-mobile-number-und-0-number","8122784828");
+		a.textBoxHandling("id","edit-field-email-address-und-0-email","tavignesh@gmail.com");
+		a.textBoxHandling("id","edit-field-present-address-und-0-name-line","Vignesh Thevendriya Anandam");
+		a.textBoxHandling("id","edit-field-present-address-und-0-thoroughfare","45/18, Michael moopanar street");
+		a.textBoxHandling("id","edit-field-present-address-und-0-premise","Kamarajar Salai");
+		a.textBoxHandling("id","edit-field-present-address-und-0-locality","Madurai");
+		a.dropDownHandling("id","edit-field-present-address-und-0-administrative-area",24);
+		a.textBoxHandling("id","edit-field-present-address-und-0-postal-code","625009");
+		a.textBoxHandling("id","edit-field-permanent-address-und-0-first-name","Vignesh");
+		a.textBoxHandling("id","edit-field-permanent-address-und-0-last-name","Thevendriya Anandam");
+		a.textBoxHandling("id","edit-field-permanent-address-und-0-thoroughfare","45/18, Michael moopanar street");
+		a.textBoxHandling("id","edit-field-permanent-address-und-0-premise","Kamarajar Salai");
+		a.textBoxHandling("id","edit-field-permanent-address-und-0-locality","Madurai");
+		a.dropDownHandling("id","edit-field-permanent-address-und-0-administrative-area",24);
+		a.textBoxHandling("id","edit-field-permanent-address-und-0-postal-code","625009");
+		a.checkBoxHandling("xpath",".//*[@id='edit-field-willing-to-work-und']/div[1]/label");
+		a.locateElement("xpath",".//*[@id='customers-node-form']/div/fieldset[2]/legend/span/a").click();
+		//need some pause : div not expand immediately - vinoth
+		a.pause(2000);
+		a.textBoxHandling("xpath",".//*[@id='edit-field-education-information-und-0-field-institution-name-und-0-value']","KLNCIT");
+		a.textBoxHandling("xpath",".//*[@id='edit-field-education-information-und-0-field-degree-name-und-0-value']","B.E");
+		a.textBoxHandling("xpath",".//*[@id='edit-field-education-information-und-0-field-specialization-und-0-value']","CSE");
+		a.dropDownHandling("xpath",".//*[@id='edit-field-education-information-und-0-field-year-of-passing-und-0-value-year']",18);
+		a.textBoxHandling("xpath",".//*[@id='edit-field-education-information-und-0-field-percentage-of-marks-und-0-value']","80");
+		a.locateElement("xpath",".//*[@id='customers-node-form']/div/fieldset[3]/legend/span/a").click();
+		//need some pause : div not expand immediately - vinoth
+		a.pause(2000);
+		a.textBoxHandling("xpath",".//*[@id='edit-field-professional-information-und-0-field-company-name-und-0-value']","CTS");
+		a.textBoxHandling("xpath",".//*[@id='edit-field-professional-information-und-0-field-role-und-0-value']","STA");
+		a.locateElement("xpath",".//*[@id='edit-field-professional-information-und-0-field-duration-und-0-value-datepicker-popup-0']").clear();
+		a.textBoxHandling("xpath",".//*[@id='edit-field-professional-information-und-0-field-duration-und-0-value-datepicker-popup-0']","5/2010");
+		a.locateElement("xpath",".//*[@id='edit-field-professional-information-und-0-field-duration-und-0-value2-datepicker-popup-0']").clear();
+		a.textBoxHandling("xpath",".//*[@id='edit-field-professional-information-und-0-field-duration-und-0-value2-datepicker-popup-0']","12/2017");
+		a.textBoxHandling("xpath",".//*[@id='edit-body-und-0-value']","HI");
+		a.buttonHandling("xpath",".//*[@id='edit-submit']");
+		//a.windowHandling("are you sure about submitting the page");
+		//a.goOutOfFrame("Create Customers | Online Software Testing Training");
+		
+		
+		
+		
+		
+		a.Ale();
+		Thread.sleep(2000);
+		
+		a.TakeScreenshot("Screenshot/creat.png");
+		test.log(LogStatus.PASS, "Vignesh Created Successfully" + test.addScreenCapture(System.getProperty("user.dir") + "/Screenshot/creat.png"));
+		a.OpenURL("http://www.learnnshine.com/drupal/manage-customers");
+		a.buttonHandling("xpath",".//*[@id='block-system-main']/div/div/div/table/tbody/tr[1]/td[4]/a[1]");
+		a.locateElement("id","edit-field-present-address-und-0-thoroughfare").clear();
+		a.locateElement("id","edit-field-present-address-und-0-premise").clear();
+		a.locateElement("id","edit-field-permanent-address-und-0-locality").clear();
+		a.locateElement("id","edit-field-present-address-und-0-postal-code").clear();
+		a.textBoxHandling("id","edit-field-present-address-und-0-thoroughfare","Plot no 109");
+		a.textBoxHandling("id","edit-field-present-address-und-0-premise","Medavakkam");
+		a.textBoxHandling("id","edit-field-present-address-und-0-locality","Chennai");
+		a.textBoxHandling("id","edit-field-present-address-und-0-postal-code","600100");
+		a.buttonHandling("xpath",".//*[@id='edit-submit']");
+		a.Ale();
+		a.pause(3000);
+		a.TakeScreenshot("Screenshot/update.png");
+		test.log(LogStatus.PASS, "Vignesh Updated Successfully" + test.addScreenCapture(System.getProperty("user.dir") + "/Screenshot/update.png"));
+		//close browser after complete the process - Vinoth
+		a.closeBrowser();
+		
+		reports.endTest(test);
+		
+		reports.flush();
+		reports.close();
+		}
+
+}
